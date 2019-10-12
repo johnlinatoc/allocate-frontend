976fd03b7fcbabc3bdc0596334d42045ebb987e4
John Linatoc <johnlinatoc1227@gmail.com>
2019-10-12 14:23:10 -0500

    Add preliminarty redux components into appropriate folders

--- src/index.js
+++ src/index.js
@@ -5,22 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import allReducers from './reducers'
 
-function reducer() {
-  return {
-    
-  }
-}
-
-const store = createStore(reducer);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <Provider store={store}>
     <Router>
       <App />
-    </Router>
-  </Provider>,
+    </Router>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
